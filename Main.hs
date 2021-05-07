3d098e3144222dde3209df4dee60e8e9e919e268
Ariel Nunez <54999+ingenieroariel@users.noreply.github.com>
2021-05-07 17:49:45 -0500

    Site with links

--- Main.hs
+++ Main.hs
@@ -73,15 +73,37 @@ frontend = O.Frontend
 
       el "h2" $ text $ "What is hDAO?"
       el "p" $ text "hDAO is the governance token for the decentralized digital assets marketplace hic et nunc"
+      el "h2" $ text $ "Facts (as of May 7, 2021)"
+      el "ul" $ do
+        el "li" $ text $ "Tezos FA2 Token"
+        el "li" $ text $ "651k total supply (no more will be generated, ever)"
+        el "li" $ text $ "Token of hicetnunc.xyz (~20k xtz 24h volume)"
+
+        el "li" $ text $ "~15 million market cap"
+        el "li" $ text $ "Largest holder < 3% of total supply"
+        el "li" $ text $ "Not yet listed on any exchange, can only be bought if you hold Tezos outside an exchange."
+
+      el "h2" $ text $ "Utility"
+      el "ul" $ do
+        el "li" $ text $ "OBJK (nft) upvoting"
+        el "li" $ text $ "hicetnunc governance (in development, to be released after Florence Tezos update lands)"
+        el "li" $ text $ "Decision on hicetnunc dividends (after governance lands)"
+     
+      -- Adapted from work by Leonard Schoelch. Hey Leo, thanks!
       el "h2" $ text $ "Useful links"
      
       el "ul" $ do
-        el "li" $  elAttr "a"
-        ("href" =: "https://github.com/hicetnunc2000/hicetnunc/wiki/hDAO" )  (text "hDAO Wiki Page")
+        el "li" $ elAttr "a" ("href" =: "https://github.com/hicetnunc2000/hicetnunc/wiki/hDAO" )  (text "hDAO Wiki Page (includes instructions on how to buy")
  
-        el "li" $  elAttr "a"
-        ("href" =: "https://hicetnunc.xyz" )  (text "hic et nunc")
+        el "li" $ elAttr "a" ("href" =: "https://hicetnunc.xyz" )  (text "hic et nunc")
+        el "li" $ elAttr "a" ("href" =: "https://tzkt.io/KT1QxLqukyfohPV5kPkw97Rs6cw1DDDvYgbB/dex" )  (text "Recent price chart")
+        el "li" $ elAttr "a" ("href" =: "https://objkts.xyz" )  (text "current price and pool volume")
+        el "li" $ elAttr "a" ("href" =: "https://app.powerbi.com/view?r=eyJrIjoiZTdmMzkxOWUtNTNlMC00ZjY2LWFiMmMtZWNmMTI5NWUyYTJlIiwidCI6IjUxM2JkYzVlLTFjMTQtNDFiYS1iZmEwLWU4NmU2MjEzZDI4ZiIsImMiOjl9" )  (text "hic et nunc sales volume")
+        el "li" $ elAttr "a" ("href" =: "https://hicetnunc.xyz/objkt/60123" )  (text "Top hDAO holders")
+        el "li" $ elAttr "a" ("href" =: "https://www.hicetnunc.xyz/tz/tz1c2iwyckUCcicx2qxqtwLEartYFEHg1pvB" )  (text "hDAO live information as NFTs, how meta.")
 
+      el "h2" $ text $ "Technical Info"
+      el "p" $ text $ "hDAO contract address KT1AFA2mwNUMNd4SsujE1YYp29vd8BZejyKW token id 0"
       el "div" $ do
       return ()
   }
@@ -91,6 +113,5 @@ backend = O.Backend
   { O._backend_run = \serve -> serve $ const $ return ()
   , O._backend_routeEncoder = fullRouteEncoder
   }
-
 
 main = O.run 8001 (O.runServeAsset "static")  backend frontend
